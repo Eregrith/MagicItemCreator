ec6424bcef215152995ded68dd093663ee2945e9
Stephane FRACHE <stephane.frache@keyconsulting.fr>
2015-02-26 14:54:10 +0100

    Ajout d'un fichier oublié

---
+++ MagicItemCreator/MagicItemCreator/Tables/Weapons/WeaponTableLine.cs
@@ -0,0 +1,38 @@
+﻿using MagicItemCreator.Creators;
+using MagicItemCreator.CustomTypes;
+using MagicItemCreator.Enums;
+using MagicItemCreator.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicItemCreator.Tables.Weapons
+{
+    //Classe abstraite correspondant au tableau en general
+    public abstract class AbstractWeaponTableLine : TableLine
+    {
+        public int AlterationBonus { get; set; }
+
+        public int Price { get; set; }
+
+        public abstract MagicWeapon Create(ItemQuality quality);
+    }
+
+    //Cas basique d'une arme +x avec un prix défini
+    public class WeaponTableLine : AbstractWeaponTableLine
+    {
+        public override MagicWeapon Create(ItemQuality quality)
+        {
+            MagicWeapon item = new MagicWeapon(quality);
+
+            item.Range = MagicItemCreation.ChosenRange;
+            item.AlterationBonus = this.AlterationBonus;
+            item.Price = this.Price;
+            item.Range = MagicItemCreation.ChosenRange;
+            item.Type = ItemType.Weapon;
+
+            return item;
+        }
+    }
+}
