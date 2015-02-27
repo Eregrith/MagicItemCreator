b829e05a6b50713b208da88f3ed0d11ec5156b9f
Stephane FRACHE <stephane.frache@keyconsulting.fr>
2015-02-27 10:55:15 +0100

    Ajout de tests

--- MagicItemCreator/UnitTests/WeaponsTests/BasicWeaponTests.cs
+++ MagicItemCreator/UnitTests/WeaponsTests/BasicWeaponTests.cs
@@ -43,5 +43,102 @@ namespace UnitTests.WeaponsTests
             weapon.Abilities.Should().BeNull();
             weapon.Range.Should().Be(chosenRange);
         }
+
+        [TestMethod]
+        public void CheckMediumWeapon()
+        {
+            //On prepare les jets :
+            // 13 -> Weapon
+            // 37 -> arme +3 à 18000po
+            MockDices dices = new MockDices(new List<int> { 13, 37 });
+
+            //Utilisation des dés pipés :P
+            MagicItemCreation.Instantiate(dices);
+
+            //Portee = melee
+            Range chosenRange = Range.Melee;
+
+            MagicItemCreation.Instance.ChosenRange = chosenRange;
+
+            //Creation
+            MagicItem item = MagicItemCreation.Instance.Create(ItemQuality.Medium);
+
+            item.GetType().Should().Be(typeof(MagicWeapon));
+
+            MagicWeapon weapon = item as MagicWeapon;
+
+            weapon.Quality.Should().Be(ItemQuality.Medium);
+            weapon.Type.Should().Be(ItemType.Weapon);
+            weapon.Price.Should().Be(18000);
+            weapon.AlterationBonus.Should().Be(3);
+            weapon.Abilities.Should().BeNull();
+            weapon.Range.Should().Be(chosenRange);
+        }
+
+        [TestMethod]
+        public void CheckMajorWeapon()
+        {
+            //On prepare les jets :
+            // 13 -> Weapon
+            // 43 -> arme +5 à 50000po
+            MockDices dices = new MockDices(new List<int> { 13, 43 });
+
+            //Utilisation des dés pipés :P
+            MagicItemCreation.Instantiate(dices);
+
+            //Portee = melee
+            Range chosenRange = Range.Melee;
+
+            MagicItemCreation.Instance.ChosenRange = chosenRange;
+
+            //Creation
+            MagicItem item = MagicItemCreation.Instance.Create(ItemQuality.Major);
+
+            item.GetType().Should().Be(typeof(MagicWeapon));
+
+            MagicWeapon weapon = item as MagicWeapon;
+
+            weapon.Quality.Should().Be(ItemQuality.Major);
+            weapon.Type.Should().Be(ItemType.Weapon);
+            weapon.Price.Should().Be(50000);
+            weapon.AlterationBonus.Should().Be(5);
+            weapon.Abilities.Should().BeNull();
+            weapon.Range.Should().Be(chosenRange);
+        }
+
+        [TestMethod]
+        public void MinorWeaponWithOneSpecialAbility()
+        {
+            //On prepare les jets :
+            //  7 -> Weapon
+            // 93 -> Special ability
+            // 78 -> Arme +2 qui coute 8000 po
+            // 89 -> capa "Throwing" qui fait +1, donc arme +2 qui coute 18000
+            MockDices dices = new MockDices(new List<int> { 7, 93, 78, 89 });
+
+            //Utilisation des dés pipés :P
+            MagicItemCreation.Instantiate(dices);
+
+            //Portee = melee
+            Range chosenRange = Range.Melee;
+
+            MagicItemCreation.Instance.ChosenRange = chosenRange;
+
+            //Creation
+            MagicItem item = MagicItemCreation.Instance.Create(ItemQuality.Minor);
+
+            item.GetType().Should().Be(typeof(MagicWeapon));
+
+            MagicWeapon weapon = item as MagicWeapon;
+
+            weapon.Quality.Should().Be(ItemQuality.Minor);
+            weapon.Type.Should().Be(ItemType.Weapon);
+            weapon.Price.Should().Be(18000);
+            weapon.AlterationBonus.Should().Be(2);
+            weapon.Abilities.Should().NotBeNull();
+            weapon.Abilities.Count.Should().Be(1);
+            weapon.Abilities[0].Should().Be("Throwing");
+            weapon.Range.Should().Be(chosenRange);
+        }
     }
 }
